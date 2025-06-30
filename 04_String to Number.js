a444d633d993afb07f3ca20c2fa58b9349f29e2f
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-07-01 05:17:42 +0530

    string & number

---
+++ 04_String to Number.js
@@ -0,0 +1,23 @@
+let value = 2
+let negValue = -value
+console.log(negValue)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + 2 + "3")
+console.log("1" - 2)
+console.log(1 - "2")
+console.log("1" + 2 + 3)
+
+console.log(+true)
+console.log(+false)
+console.log(+null)
+console.log(+undefined)
+console.log(+"")
+
+let num1, num2, num3
+num1 =num2=num3=1+ 1
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num1 === num2)
