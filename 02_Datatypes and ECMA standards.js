d64530c30dcebfaa8bb43f2d2417c6f04bd8f97e
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-06-30 22:10:28 +0530

    Datatypes

---
+++ 02_Datatypes and ECMA standards.js
@@ -0,0 +1,19 @@
+"use strict"
+alert(3+3)
+
+//number - 2 power 53
+// bigint
+// string - ""
+//boolean - t/f
+//null - empty value
+// symbol - used in components [unique value]
+//object - array, function, date, regex
+//undefined - no value
+// let - block scope
+// var - function scope
+// const - block scope
+
+let accountId = 144
+console.log(typeof accountId)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
