e2c2ec095d88fe35be54ed146cedc3ba3db33f73
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-07-03 09:13:27 +0530

    datatypes summary

---
+++ 06_Summary of Datatypes.js
@@ -0,0 +1,42 @@
+// Primitive 
+// 7 types :
+// String
+// Number 
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+
+// Number is numnber no decimal or float
+
+// const scoreValue = 100.3
+// const score = 100
+
+// when no value assigned it contains undefined
+
+// const abc = Symbol("abc")
+// const ABC = Symbol("abc")
+// console.log(abc === ABC)  false
+//  console.log(abc == ABC)  false
+
+// const bigNumber = 5495273475324875834785732402348758347n
+
+// Reference Type(nON Primitive):
+
+// Array
+// Object
+// Functions
+
+// const place = ["winden","forks",]
+// let myObject = {
+//     name: zaid,
+//     age: 23,
+// }
+
+// const myFunction = function () {
+//     console.log("Hello World")
+// }
+
+// //function typeof function
