55e4706cf13300b81e64f95cc296655a26764fba
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-07-21 01:05:42 +0530

    while and do while

---
+++ 24_while und do while.js
@@ -0,0 +1,18 @@
+let index = 0
+while(index <= 10){
+    console.log(`value of index is ${index}`)
+    index++
+}
+
+let myArr = ['flash','batman','superma']
+let arr = 0
+while(arr< myArr.length){
+    console.log(myArr[arr])
+    arr++
+}
+
+let score =11
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score <=10)
