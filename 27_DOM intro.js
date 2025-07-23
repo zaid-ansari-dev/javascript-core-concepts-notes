682af3d5e54117ff53e7710aaf5d45cd24e21e89
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-07-24 00:44:06 +0530

    dom

---
+++ 27_DOM intro.js
@@ -0,0 +1,10 @@
+//DOM -- document object model
+//console.log(window)
+//console.log(window.document)
+//console.log(document)
+//console.dir(document)
+//console.log(document.baseURI)
+//console.log(document.links)
+//console.log(document.links[2])
+//document.getElementById("firstHeading")
+//document.getElementById("firstHeading").innerHTML = <h1>zaid</h1>
